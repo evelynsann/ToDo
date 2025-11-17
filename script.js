2333d5804028a670e553e41f33ab3279cbd0abcf
evelynsann <evelyndossantos0312@gmail.com>
2025-11-17 17:21:28 -0300

    todo

--- script.js
+++ script.js
@@ -33,12 +33,25 @@ function iniciaToDo() {
     // Associa função "adicionarTarefaEnter()" ao evento de pressionar a tecla "Enter"
     //  no campo de "Adicionar" nova tarefa
     txt_nova_tarefa.addEventListener("keypress", adicionarTarefaEnter) 
+
+    const arrayTarefas = obterTarefasDoNavegador();
+
+    salvarCookieTarefas([]);
+    arrayTarefas.forEach(strTarefa => {
+        adicionarTarefa(strTarefa);
+    });
+
+    lista_tarefas.querySelectorAll("li").forEach(li => makeDraggable(li));
 }
 
-function adicionarTarefa() {
+function adicionarTarefa(strTarefa) {
+    if (typeof strTarefa !== 'string' || strTarefa == null) {
+        strTarefa = txt_nova_tarefa.value;
+    }
+
     // Se a caixa de texto de "Adicionar nova tarefa" não está vazia
     // .trim() remove espaços em branco do começo e fim do valor do campo
-    if (txt_nova_tarefa.value.trim() !== "") {
+    if (strTarefa.trim() !== "") {
         const btn_item = `
         <div>
         <button class="btn btn-success btn-sm me-2 btn-concluir" onclick="concluirTarefa(this)">Concluir</button>
@@ -53,8 +66,20 @@ function adicionarTarefa() {
         // "span" permite aolicar formatações em linha
         // "w-75" limita o nome da tarefaa 75% do tamanho da linha, deixando 25% da largura restante reservado para os botões 
         // "text-truncate" corta e adiciona reticências (três pontos ...) em nomes de tarefas que excedem 75% da largura da linha
-        item.innerHTML = "<span class='w-75 text-truncate'>" + txt_nova_tarefa.value + "</span>" + btn_item;
+        item.innerHTML = "<span class='w-75 text-truncate'>" + strTarefa + "</span>" + btn_item;
         
+        makeDraggable(item);
+        item.addEventListener("dragend", () => {
+            let arrayTarefas = [];
+            Array.from(lista_tarefas.children).forEach(i => {
+                i.classList.remove('over')
+                arrayTarefas.push(i.querySelector("span").textContent);
+            });
+            salvarCookieTarefas(arrayTarefas);
+        });
+
+        adicionarTarefaAoCookie(strTarefa);
+
         // Adiciona o item a lista de tarefas
         lista_tarefas.append(item);
     }
@@ -90,7 +115,11 @@ function concluirTarefa(btn_concluir) {
     excluirTarefa();
 }
 
-function excluirTarefa() {
+function excluirTarefa() { 
+    const arrayTarefas =  obterTarefasDoNavegador();
+    arrayTarefas.splice(id_tarefa_excluir, 1);
+    salvarCookieTarefas(arrayTarefas);
+
     // Remove o item da lista de tarefas
     lista_tarefas.removeChild(lista_tarefas.children[id_tarefa_excluir]);
     // Fecha a modal de "Excluir tarefa"
@@ -109,9 +138,44 @@ function obterIDTarefaExcluir(btn) {
     id_tarefa_excluir = tarefas.indexOf(item);
 }
 
-
 // -----------------------------------------------------------------
-// 3. ESCUTADORES DE EVENTOS E INÍCIO
+// 3. COOKIES
+// Adiciona funcionalidade de cookies (persistência) das tarefas adicionadas
+// (mantém as tarefas adicionadas mesmo ao fechar ou atualizar a página)
+// -----------------------------------------------------------------
+
+const CHAVE_TAREFAS_TODO = 'tarefas_todo';
+
+function obterTarefasDoNavegador() {
+    // Tenta ler o cookie do navegador
+    try {
+        const cookie = localStorage.getItem(CHAVE_TAREFAS_TODO);
+        if (cookie) {
+            // Se o cookie existir, retorna o cookie
+            return JSON.parse(cookie);
+        }
+    } catch (e) {
+        console.error("Falha ao ler o cookie do armazenamento local.");
+    }
+    // Retorna um vetor vazio em caso de falha
+    return [];
+}
+
+function salvarCookieTarefas(arrayTarefas) {
+    try {
+        localStorage.setItem(CHAVE_TAREFAS_TODO, JSON.stringify(arrayTarefas));
+    } catch (e) {
+        console.error("ERRO: Falha ao salvar as tarefas no navegador. Erro:", e);
+    }
+}
+
+function adicionarTarefaAoCookie(strTarefa) {
+    const arrayTarefas = obterTarefasDoNavegador(); // Obtém as tarefas atuais do cookie do navegador Web em formato de vetor.
+    arrayTarefas.push(strTarefa); // Adiciona a tarefa recebida como parâmetro da função ao cookie do navegador Web.
+    salvarCookieTarefas(arrayTarefas); // Salva o cookie com a tarefa adicionada no navegador Web.
+}
+// -----------------------------------------------------------------
+// 4. ESCUTADORES DE EVENTOS E INÍCIO
 // -----------------------------------------------------------------
 
 iniciaToDo();
